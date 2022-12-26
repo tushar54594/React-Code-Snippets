8343b500cd328c76dc1b44f8ff485e27152bbf65
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-12-26 18:46:07 +0530

    Update ConditionalRendering.jsx

--- ConditionalRendering.jsx
+++ ConditionalRendering.jsx
@@ -11,3 +11,12 @@ function App() {
 export default App;
 
 //Method 2
+//We can use a double ampersand(&&) to do an if statement instead of if-else
+function App() {
+    const age = 17;
+  return (
+    <div className='app'>
+      {age >= 18 && <h1>You can vote</h1>} 
+    </div>
+  )
+}
