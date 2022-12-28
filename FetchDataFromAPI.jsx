e1191a2bcb75223c2a02f54bdc73fc80f519c4d3
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-12-28 12:32:39 +0530

    Create FetchDataFromAPI.jsx

---
+++ FetchDataFromAPI.jsx
@@ -0,0 +1,18 @@
+//Method 1: Using the fetch() function
+//the fetch function returns a promise 
+
+function App() {
+  fetch("https://catfact.ninja/fact")
+.then((res) => res.json())
+.then((data) => {
+  console.log(data)
+})
+  return (
+    <div className="App">
+     <button>Generate Cat Fact</button>
+     <p> </p>
+    </div>
+  );
+}
+
+//Method 2: Using the Axios Library
