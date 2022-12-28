31a89632f251a16df478076b3e7a89f819929317
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-12-28 16:24:56 +0530

    Create ReactRouterDOM.jsx

---
+++ ReactRouterDOM.jsx
@@ -0,0 +1,48 @@
+//First install React Router dom
+npm install react-router-dom
+
+//import the necessary components
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+//The Route component is self closing and we need to specify 2 things : path and element
+
+//Example of Navbar
+
+//NAVBAR Component
+import { Link } from "react-router-dom";
+export const Navbar = () => {
+  return (
+    <div>
+      <Link to="/"> Home </Link>
+      <Link to="/menu"> Menu </Link>
+      <Link to="/contact"> Contact </Link>
+    </div>
+  );
+};
+
+//App.js File
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
